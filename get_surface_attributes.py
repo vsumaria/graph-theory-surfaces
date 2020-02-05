96e6fa090293280ff1dae41c542d7c105eebc02a
Siddharth Deshpande <deshpan5@halstead-fe01.rcac.purdue.edu>
2020-02-05 11:08:57 -0500

    Added new dist_edge feature to edges in chem_env_PDH.py, calculating the CN for ads and nearest neighbor as well as getting distance matrix for all the nearest neighbors.

--- get_surface_attributes.py
+++ get_surface_attributes.py
@@ -1,18 +1,21 @@
 from ase.io import read
 import numpy as np
-from chem_env_sid import *
+from chem_env_PDH_surf import *
 from networkx import *
 from sys import argv
 from ase.visualize import view
 import itertools
 
+
 def add_to_list(graph,g_node):
+    bond_distance = []
     x = []
     for i in graph.neighbors(g_node):
         if 'ads' not in graph.nodes[i]:
             x.append(i)
-
-    return x
+            bond_distance.append(graph.edges[g_node,str(i)]['dist_edge'])
+            
+    return x,bond_distance
 
 
 def get_ads_list(graph):
@@ -35,18 +38,24 @@ nl.update(atoms)
 full, chem_env = process_atoms(atoms,nl,adsorbate_atoms,radius=2)
 ads_nodes = get_ads_list(chem_env[0])
 nearest_neighbor = []
+bond_distance = []
 for i in ads_nodes:
-    nn = add_to_list(chem_env[0],i)
+    nn,bond_distances = add_to_list(chem_env[0],i)
     nearest_neighbor.append(nn)
+    bond_distance.append(bond_distances)
 
 nearest_neighbor=(list(itertools.chain.from_iterable(nearest_neighbor)))
+bond_distance=(list(itertools.chain.from_iterable(bond_distance)))
 print(nearest_neighbor)
-
+print(bond_distance)
 next_nearest = []
+bond_distance =[]
 for i in nearest_neighbor:
-    nn = add_to_list(chem_env[0],i)
+    nn,bond_distances = add_to_list(chem_env[0],i)
     next_nearest.append(nn)
+    bond_distance.append(bond_distances)
 print(next_nearest)
-#next_nearest_neighbor = add_to_list(chem_env[0],nearest_neighbor)
-
-#print(ads_nodes,nearest_neighbor,next_nearest_neighbor)
+print(bond_distance)
+CN_ads = len(nearest_neighbor)
+CN_site = np.mean(list(len(i) for i in next_nearest))
+print(CN_ads, CN_site)

ca89b1c905b73616bf442ece1844c26e445838f5
tgmaxson <tgmaxson@gmail.com>
2021-01-25 02:56:04 -0500

    Final setup.py for 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README", "r") as readme:
 
 setuptools.setup(
         name="surfgraph",
-        version="0.1",
+        version="0.1.1",
         scripts=['bin/analyze_chem_env.py', 'bin/generate_sites.py'],
         author="Tristan Maxson, Siddharth Deshpande",
         author_email="tgmaxson@gmail.com, sidd20111992@gmail.com",
@@ -14,6 +14,13 @@ setuptools.setup(
         long_description=long_description,
         long_description_content_type="text/markdown",
         url="https://gitlab.com/jgreeley-group/graph-theory-surfaces",
+        python_requires=">=3",
+        install_requires=[
+            'numpy>=1.11.3',
+            'networkx>=2.5',
+            'matplotlib>=2.0.0',
+            'ase>=19.1',
+        ],
         packages=setuptools.find_packages(),
         classifiers=[
             "Intended Audience :: Science/Research",
@@ -21,6 +28,8 @@ setuptools.setup(
             "Topic :: Scientific/Engineering :: Chemistry",
             "Topic :: Scientific/Engineering :: Physics",
             "Topic :: Scientific/Engineering",
+            'License :: OSI Approved :: '
+            'GNU Lesser General Public License v2 or later (LGPLv2+)',
             "Framework :: Matplotlib"],
         )
 
